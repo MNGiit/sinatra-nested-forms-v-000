acc263bda95e553522eaf8271ecdd5e87be0a9b8
Mohammed Noori <kmanoorib@gmail.com>
2018-12-27 22:24:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,12 +4,23 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-    get "/" do
+    get '/' do
       erb :root
     end
-    
-    get "/new" do
-      erb :"pirates/new"
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
     end
     
   end # end of class
